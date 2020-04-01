1951fc8d0130b83eff1096c9c762a2d641285819
Nathan Hill <nateohill@gmail.com>
2020-04-01 11:35:00 -0500

    Started adding in navigation

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
-import "react-native-gesture-handler";
 import React from "react";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import "react-native-gesture-handler";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet } from 'react-native';
+
 import { ThemeProvider } from "react-native-elements";
 import { Provider } from "react-redux";
 
@@ -13,26 +14,44 @@ import Home from "./src/screens/Home";
 import DeaconsList from "./src/screens/DeaconsList";
 import FamiliesList from "./src/screens/FamiliesList";
 import VisitationList from "./src/screens/VisitationList";
+import ProfileButton from "./src/components/ProfileButton";
 
-const Tab = createBottomTabNavigator();
+const styles = StyleSheet.create({
+  ProfileButtonTitle: {
+    display: "none"
+  },
+  ProfileButton: {
+    height: 10,
+  }
+});
+
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <ThemeProvider>
-      <NavigationContainer>
-        <Provider store={store}>
-          <Stack.Navigator>
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="Home"
+
+          >
             <Stack.Screen name="Profile" component={Profile} />
+            <Stack.Screen
+              name="ProfileButton"
+              component={ProfileButton}
+              options={{
+                headerTransparent: true,
+                headerTitleStyle: styles.ProfileButtonTitle,
+              }}
+            />
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Deacons List" component={DeaconsList} />
+            <Stack.Screen name="Families List" component={FamiliesList} />
+            <Stack.Screen name="Visitation List" component={VisitationList} />
           </Stack.Navigator>
-          <Tab.Navigator>
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Deacons List" component={DeaconsList} />
-            <Tab.Screen name="Families List" component={FamiliesList} />
-            <Tab.Screen name="Visitation List" component={VisitationList} />
-          </Tab.Navigator>
-        </Provider>
-      </NavigationContainer>
+        </NavigationContainer>
+      </Provider>
     </ThemeProvider>
   );
 }
