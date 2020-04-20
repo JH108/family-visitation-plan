018de6c5350fcd92bf167a32b285ad2d3150ca55
Nathan Hill <nateohill@gmail.com>
2020-04-20 15:23:53 -0500

    Mapped out the families similar to how deacons are mapped out

--- src/screens/FamiliesList.tsx
+++ src/screens/FamiliesList.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
-import { Text, SafeAreaView } from "react-native";
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+import { useSelector } from 'react-redux';
+import { BasicCard } from '../components/Cards';
+import { Families, Family } from '../redux-modules/families';
+import { ScrollView } from 'react-native-gesture-handler';
+
+const styles = StyleSheet.create({
+  centeredContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 15,
+  },
+});
 
 
 const FamiliesList = ({ navigation }) => {
+  const families: Families = useSelector((state) => state.familiesSlice);
+  console.log(families);
   return (
     <SafeAreaView>
       <Text>Families List</Text>
+      <ScrollView contentContainerStyle={styles.centeredContainer}>
+        {families.map((family: Family) => {
+          return (
+            <BasicCard
+              key={family.id}
+              title={family.familyName}
+              subtitle={family.deacon}
+              phoneNumber={family.homePhone}
+              families={[]}
+            />
+          );
+        })}
+      </ScrollView>
     </SafeAreaView>
   );
 };
