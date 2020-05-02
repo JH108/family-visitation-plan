1e05e5fe6bcb227c408f2e290b7591b178236132
Nathan Hill <nateohill@gmail.com>
2020-05-02 12:29:20 -0500

    Added type definitions for people and visits Revised definitions for families and deacons Implemented new definitions in the families slice Delayed implementing new deacon types definition

---
+++ src/typescript/Deacon.ts
@@ -0,0 +1,7 @@
+export interface Deacon {
+  id: string;
+  personId: string;
+  familyId: string;
+  families: Array<string>;
+  visits: Array<string>;
+}
---
+++ src/typescript/Visit.ts
@@ -0,0 +1,9 @@
+export interface Visit {
+  id: string;
+  deaconId: string;
+  familyId: string;
+  date: Date;
+  reason: string;
+  notes: string;
+  typeOfVisit: string;
+}
