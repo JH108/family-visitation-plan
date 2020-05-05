985450df06e3e1e2d7271a2140552ba4dcd3252c
Nathan Hill <nateohill@gmail.com>
2020-05-05 10:42:31 -0500

    Started implementing family creation form

--- src/screens/FamiliesList.tsx
+++ src/screens/FamiliesList.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, SafeAreaView, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 import { BasicCard } from '../components/Cards';
-import { Families, Family } from '../redux-modules/families';
+import { Families } from '../redux-modules/families';
+import { Family } from '../typescript/Family';
 import { ScrollView } from 'react-native-gesture-handler';
+import { Modal, Button } from 'react-native-paper';
+import Form from '../components/Form';
+import { FieldTypes } from '../components/Form/index';
 
 const styles = StyleSheet.create({
   centeredContainer: {
@@ -13,20 +17,47 @@ const styles = StyleSheet.create({
   },
 });
 
-
 const FamiliesList = ({ navigation }) => {
   const families: Families = useSelector((state) => state.familiesSlice);
-  console.log(families);
+  const [visible, setVisible] = useState(false);
   return (
     <SafeAreaView>
       <Text>Families List</Text>
+      <Button onPress={() => {
+        setVisible(!visible);
+        return (
+          <Modal
+            visible={visible}
+            children={
+              <Form
+                onDismiss={() => ''}
+                fields={
+                  Object.keys(families).map((family) => {
+                    console.log(family);
+                    return {
+                      name: 'asdf',
+                      label: '',
+                      type: FieldTypes.INPUT,
+                      required: false
+                    };
+                  })
+                }
+                formName="New Family"
+                onSubmit={(e) => {
+
+                }
+                }></Form>
+            }
+          ></Modal>
+        );
+      }}>Add Family</Button>
       <ScrollView contentContainerStyle={styles.centeredContainer}>
         {families.map((family: Family) => {
           return (
             <BasicCard
               key={family.id}
               title={family.familyName}
-              subtitle={family.deacon}
+              subtitle={family.familyName}
               phoneNumber={family.homePhone}
               families={[]}
             />
