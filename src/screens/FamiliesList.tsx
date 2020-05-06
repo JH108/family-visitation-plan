7f5097cdd35d4002b0f12bacd5b916d7f661b44f
Nathan Hill <nateohill@gmail.com>
2020-05-06 13:19:58 -0500

    Changed passing of props to BasicCard to be correct

--- src/screens/FamiliesList.tsx
+++ src/screens/FamiliesList.tsx
@@ -5,6 +5,7 @@ import { BasicCard } from '../components/Cards';
 import { Families } from '../redux-modules/families';
 import { Family } from '../typescript/Family';
 import { ScrollView } from 'react-native-gesture-handler';
+import { Deacons } from '../redux-modules/deacons';
 
 const styles = StyleSheet.create({
   centeredContainer: {
@@ -17,17 +18,18 @@ const styles = StyleSheet.create({
 
 const FamiliesList = ({ navigation }) => {
   const families: Families = useSelector((state) => state.familiesSlice);
-  console.log(families);
+  const deacons: Deacons = useSelector((state) => state.deaconsSlice);
   return (
     <SafeAreaView>
       <Text>Families List</Text>
       <ScrollView contentContainerStyle={styles.centeredContainer}>
         {families.map((family: Family) => {
+          // Find deacon by id and pass the name
           return (
             <BasicCard
               key={family.id}
               title={family.familyName}
-              subtitle={family.deacon}
+              subtitle={family.familyName}
               phoneNumber={family.homePhone}
               families={[]}
             />
