44fdd69fe4a0f550914361dad451cb3092de418c
Nathan Hill <nateohill@gmail.com>
2020-05-11 10:20:52 -0500

    Added a custom theme type

--- App.tsx
+++ App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 
-import { ThemeProvider } from 'react-native-elements';
-import { Provider as PaperProvider, Portal } from 'react-native-paper';
+import { ThemeProvider, useTheme } from 'react-native-paper';
+import { Provider as PaperProvider, Portal, DefaultTheme } from 'react-native-paper';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
@@ -11,11 +11,32 @@ import Navigator from './src/Navigator';
 
 import { store, persistor } from './src/redux-modules/store';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import CustomTheme from './src/typescript/CustomTheme';
+
+export const useCustomTheme = () => {
+	const theme = useTheme();
+
+	return theme as CustomTheme;
+};
+
+const theme: CustomTheme = {
+	...DefaultTheme,
+	fontSizes: {
+		small: 16,
+		medium: 24,
+		large: 32
+	},
+	iconSizes: {
+		small: 21,
+		medium: 29,
+		large: 37,
+	},
+};
 
 export default function App() {
 	return (
 		<PaperProvider>
-			<ThemeProvider>
+			<ThemeProvider theme={theme}>
 				<SafeAreaProvider>
 					<Provider store={store}>
 						<PersistGate loading={null} persistor={persistor}>
